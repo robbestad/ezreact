90cd8fe78d1e68e3388db5ef0fb6134f10c3f811
Sven A Robbestad <robbestad@gmail.com>
2015-12-14 23:14:24 +0100

    Heroku deploy

--- source/app.js
+++ source/app.js
@@ -9,7 +9,8 @@ class App extends Component {
   render(){
     return <div>
       <h1>Minimal Webpack+React+Babel</h1>
-      <div>This is the most minimalistic ReactJS config imaginable. It contains React, Webpack and ES2015 and is only 37.7KB gzipped. It also comes equipped with a ready-2-go express config.
+      <div>This is the most minimalistic ReactJS config imaginable. It contains React, Webpack and ES2015 and is only 38KB gzipped.</div>
+      <div> It also comes equipped with a ready-2-go express config.
       </div>
       <div>
         To use: download, install node modules and then <strong>npm start</strong>
@@ -17,6 +18,11 @@ class App extends Component {
       <div>
         Source: <a href="https://github.com/svenanders/minimal-react">github.com/svenanders/minimal-react</a>
       </div>
+      <h3>Stats</h3>
+      <div>- Babel 6</div>
+      <div>- React 0.14</div>
+      <div>- Webpack</div>
+      <div>Size: 38KB</div>
     </div>
   }
 }
