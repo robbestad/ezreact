fe039135042c36ddc49a93422742a8ca037190b2
Sven A Robbestad <robbestad@gmail.com>
2015-12-14 23:11:32 +0100

    Heroku deploy

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ var app = express();
 var port = process.env.PORT || 8080;
 var path = require("path");
 var staticGzip =  require('http-static-gzip-regexp')
-
 app.use(staticGzip(/(\.ico|\.html|\.js|\.css)$/));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", function (req, res) {
   var file = path.join(__dirname, "public", req.path);
-  //res.set('Content-Encoding', 'gzip');
-  
   res.sendFile(file);
 });
 
-app.listen(port, function () {
-});
+app.listen(port, function () {});
